016c1e340ec78c0082d1562a44c8c115de490a57
Andy McKay <amckay@mozilla.com>
2014-06-30 07:45:57 -0700

    lets stand something up at least

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name='blink',
-    version='0.1',
+    version='0.0.1',
     description='REST client interface',
     long_description=open('README.rst').read(),
     author='Andy McKay',
     author_email='andym@mozilla.com',
     license='BSD',
     install_requires=['requests'],
-    packages=['client'],
+    packages=['blink'],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
---
+++ tests/tests.py
@@ -0,0 +1,128 @@
+# -*- coding: utf8 -*-
+import uuid
+from unittest import main, TestCase
+from unittest.mock import patch
+from urllib.parse import urlparse
+
+from blink.cache import DictCache
+from blink.resource import Request, Resource
+from blink.pool import Pool
+
+
+class FakeRequest(object):
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+
+
+class FakeResponse(object):
+    pass
+
+
+class FakeRequests(TestCase):
+
+    def setUp(self):
+        patcher = patch('requests.get')
+        self.mock = patcher.start()
+        self.addCleanup(patcher.stop)
+
+
+class TestCache(TestCase):
+
+    def setUp(self):
+        self.cache = DictCache()
+        self.request = FakeRequest()
+        self.response = FakeResponse()
+
+    def test_good(self):
+        self.cache.add_etag('f', self.request, self.response)
+        assert self.cache.get_response(self.request) == self.response
+
+    def test_no_etag(self):
+        self.cache.add_etag('f', self.request, self.response)
+        self.request.id = 'b'
+        assert self.cache.get_response(self.request) == None
+
+    def test_etag(self):
+        self.cache.add_etag('f', self.request, self.response)
+        assert self.cache.get_etag(self.request) == 'f'
+
+
+class TestRequest(FakeRequests):
+
+    def test_server(self):
+        for s in ('/', 'http://', 'f.c'):
+            with self.assertRaises(ValueError):
+                Request(urlparse(s))
+
+    def test_merge(self):
+        for a, b, c in (
+                ['http://f.c/', '/f', 'http://f.c/f'],
+                ['http://f.c/b', '/f', 'http://f.c/f'],
+                ['http://f.c/b?f', 'f', 'http://f.c/f?f'],
+            ):
+            res = Request(urlparse(a)).merge(b)
+            assert res == c, res
+
+    def test_id(self):
+        for a in ('/f/b', 'Азәрбајҹан'):
+            req = Request(urlparse('http://f.c'))
+            req.set_id(a)
+
+    def test_get(self):
+        req = Request(urlparse('http://f.c'))
+        req.GET('/b')
+        self.mock.assert_called_with('http://f.c/b')
+
+
+class TestPool(TestCase):
+
+    def setUp(self):
+        self.servers = ('http://f.b/', 'http://b.c/', 'https://g.p')
+        self.pool = Pool(self.servers)
+
+    def test_next(self):
+        assert self.pool.get() == self.servers[0]
+        assert self.pool.get() == self.servers[1]
+        assert self.pool.get() == self.servers[2]
+        assert self.pool.get() == self.servers[0]
+
+    def test_disable(self):
+        self.pool.disable('http://b.c/')
+        assert len(self.pool.active) == 2
+        assert 'http://b.c/' in self.pool.inactive
+
+    def test_disable_missing(self):
+        self.pool.disable('http://b.c/')
+        self.pool.disable('http://b.c/')
+        assert len(self.pool.active) == 2
+
+    def test_disable_messed_up(self):
+        with self.assertRaises(AssertionError):
+            self.pool.disable('http://x.c/')
+
+    def test_enable(self):
+        self.pool.disable('http://b.c/')
+        self.pool.enable('http://b.c/')
+        assert len(self.pool.active) == 3
+        assert 'http://b.c/' in self.pool.active
+
+    def test_enable_missing(self):
+        self.pool.enable('http://b.c/')
+        assert len(self.pool.active) == 3
+
+    def test_enable_messed_up(self):
+        with self.assertRaises(AssertionError):
+            self.pool.disable('http://x.c/')
+
+
+class TestResource(FakeRequests):
+
+    def test(self):
+        res = Resource('http://f.com')
+        res.config()
+        res.process('GET', '/b')
+
+
+if __name__=='__main__':
+    main()
